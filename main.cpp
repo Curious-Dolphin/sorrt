d153ea5f59a66036658c5a34734c6ba231811ffc
Filippo <filchio.fc@gmail.com>
2023-03-02 19:17:50 +0100

    bubble

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
-kek
+#include <array>
+#include <iostream>
+
+#define MAX 6
+
+void bubbleSort(std::array<int, MAX>& input){
+    for(auto ex_it = input.begin(); ex_it!= input.end(); ex_it++){
+        for (auto it = input.begin(); it!= input.end(); it++){
+            if(*it> *(it+1)){
+                *it= *it+ *(it+1);
+                *(it+1) = *it- *(it+1);
+                *it = *it - *(it+1);
+            }
+        }
+    }
+}
+
+void selectionSort(){
+    
+}
+
+
+int main(){
+    std::array<int, MAX> prova{2,4,1,6,7,7};
+    bubbleSort(prova);
+        for (auto i : prova){
+        std::cout << i  << "\n";
+    }    
+
+    return 0;
+}
